7ef3f47c0cb9dafb9c018a1215f775b8febbbea3
akihirosasaki <akihirosasaki0730@gmail.com>
2017-05-15 19:22:33 +0900

    Update Myinterpolation.jl

--- src/Myinterpolation.jl
+++ src/Myinterpolation.jl
@@ -1,25 +1,19 @@
-{\rtf1\ansi\ansicpg932\cocoartf1404\cocoasubrtf340
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 function myinterpolationA(grid, vals)\
-    function func(x)\
-        if x <= grid[1]\
-            return "error"\
-        elseif x >= grid[length(grid)]\
-            return "error"\
-        else\
-            index_1 = searchsortedfirst(grid, x)\
-            index_2 = searchsortedlast(grid, x)\
-        end\
-        x_1 = grid[index_1]\
-        x_2 = grid[index_2]\
-        y_1 = vals[index_1]\
-        y_2 = vals[index_2]\
-        \
-        y = ((y_2- y_1)/(x_2 - x_1))*(x - x_1) + y_1\
-        return y\
-    end\
-end}
+function myinterpolationA(grid, vals)
+    function func(x)
+        if x <= grid[1]
+            return "error"
+        elseif x >= grid[length(grid)]
+            return "error"
+        else
+            index_1 = searchsortedfirst(grid, x)
+            index_2 = searchsortedlast(grid, x)
+        end
+        x_1 = grid[index_1]
+        x_2 = grid[index_2]
+        y_1 = vals[index_1]
+        y_2 = vals[index_2]
+        
+        y = ((y_2- y_1)/(x_2 - x_1))*(x - x_1) + y_1
+        return y
+    end
+end
