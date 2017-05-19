cdd2fbc6295cc7db470e4c3032b852416aad261b
佐々木彬弘 <sasakiakihiro@sasakiakirahiroshi-no-MacBook-Air.local>
2017-05-19 14:47:26 +0900

    519

--- src/MyinterpolationA.jl
+++ src/MyinterpolationA.jl
@@ -3,14 +3,17 @@ export myinterpolationA
 
 function myinterpolationA(grid, vals)
     function func(x)
-        if x < grid[1]
+        if x < grid[1] || x > grid[length(grid)]
             return "error"
-        elseif x > grid[length(grid)]
-            return "error"
-        else
-            num_1 = searchsortedfirst(grid, x)
-            num_2 = searchsortedlast(grid, x)
         end
+        
+        
+        num_2 = searchsortedfirst(grid, x)
+        if num_2 == 1
+            return vals[num_2]
+        end
+        
+        num_1 = num_2 - 1
         x_1 = grid[num_1]
         x_2 = grid[num_2]
         y_1 = vals[num_1]
